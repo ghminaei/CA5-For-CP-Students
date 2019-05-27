37ddc1b3f4e9a0c97331e6ab01c01147b30ec401
Ghazal <ghminaei1037@gmail.com>
2019-05-27 20:31:22 +0430

    Base file added

---
+++ CA5.py
@@ -0,0 +1,65 @@
+#a helper function just for you :)
+def gcd(a ,b):
+    if(b == 0): 
+        return a 
+    else: 
+        return gcd(b, a%b) 
+"""
+Implement this class carefully.
+Please design it first on a paper then start coding.
+Think carefully about public and private methods and attributes.
+
+This homework is so easy:) So it is really important to design class carefully.
+Thanks and good luck
+"""
+class Fraction:
+    #your code
+        
+    def __init__(self, n, d):
+        #your code
+        pass
+
+    def __str__(self):
+        #your code
+        pass
+    
+    #add methodes
+    #your code
+
+
+#test code  
+while(True):
+    op = input()
+    if op == "exit":
+        #your code
+        break
+    
+    elif op == "inv":
+        #your code
+        pass
+
+    elif op == "add":
+        #your code
+        pass
+
+    elif op == "min":
+        #your code
+        pass
+
+    elif op == "mul":
+        #your code
+        pass
+
+    elif op == "div":
+        #your code
+        pass
+
+    else:
+        print("Bishtar deghat kon!")
+    
+
+
+
+
+        
+
